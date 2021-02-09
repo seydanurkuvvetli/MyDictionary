bc9ba044fd7680d467134206ff264b0764a5521f
Administrator <Administrator@DESKTOP-49AFC58>
2021-02-09 14:15:44 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, T>
+    {
+
+        T[] items;
+        K[] keys;
+        public MyDictionary()
+        {
+
+            items = new T[0];
+            keys = new K[0];
+
+
+        }
+        public void Add(K key, T item)
+        {
+
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+
+            }
+            items[items.Length - 1] = item;
+            K[] tempArray2 = keys;
+            keys = new K[keys.Length + 1];
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                keys[i] = tempArray2[i];
+
+            }
+            keys[keys.Length - 1] = key;
+
+        }
+        public void List()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine(keys[i] + "-" + items[i]);
+            }
+        }
+
+    }
+
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictioanary<int, string> myDictionary= new MyDictioanary<int, string>();
+            myDictionary.Add(1, "istanbul");
+            myDictionary.Add(2, "Rize");
+            myDictionary.Add(3, "Trabzon");
+            myDictionary.Add(4, "Samsun");
+
+
+            myDictionary.List();
+        }
+    }
+}
